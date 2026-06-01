9b8b6b48c87f48ad5bb4015aac92f2b6ccea7adc
2024125077 <323yunseo323@gmail.com>
2026-06-01 15:59:51 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,12 +4,13 @@ int main()
 {
     char arr[10] = {};
     int maxCount = 0;
-    char maxA = 0;
-    int count = 0;
+    char maxA = '\0';
     
-    scanf("%s", arr);
+    for (int *p = arr; p < arr + 10; p++) scanf("%c", p);
     
     for (char *p1 = arr; p1 < arr + 10; p1++) {
+
+        int count = 0;
         for (char *p2 = arr; p2 < arr + 10; p2++) {
             if (*p1 == *p2) {
                 count++;
@@ -19,8 +20,6 @@ int main()
                 }
             }
         }
-        count = 0;
-        
     }
     
     printf("%c %d", maxA, maxCount);
