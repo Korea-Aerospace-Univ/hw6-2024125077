a69c1f8af35aac3d3f65dea148e5dff8707d9af3
2024125077 <323yunseo323@gmail.com>
2026-06-01 15:46:20 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int x[50] = {};
+    int y[50] = {};
+    
+    int N = 0;
+    scanf("%d", &N);
+    
+    for (int *p = x; p < x + N; p++) scanf("%d", p);
+    for (int *q = y; q < y + N; q++) scanf("%d", q);
+    
+    int *px = x; 
+    int *py = y + N -1;         
+    
+    for (int i = 0; i < N; i++) {
+        printf("%d ", (*px) + (*py)); 
+        px++;
+        py--;
+    }
+}
